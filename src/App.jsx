e836513930815507885b6d4116623c6bf869bc87
Andrew Denta <andrew.denta@gmail.com>
2023-12-08 14:59:04 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import logo from "./assets/logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+          <img id="logo" src={logo} />
+         <p style={{fontSize:'20px', lineHeight:'2'}}>Coming Soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
